5d71858a29f87242409c0324762d295d368e15ed
tsukhu <tsukhu@hcl.com>
2017-03-08 22:26:05 +0530

    Md sample app first draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { AppComponent } from './app.component';
 import { APP_RESOLVER_PROVIDERS } from './app.resolver';
 import { AppState, InternalStateType } from './app.service';
 import { HomeComponent } from './home';
+import { LeashedInHomeComponent, SettingsDialogComponent } from './leashedInHome';
 import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
 import { XLargeDirective } from './home/x-large';
@@ -58,8 +59,14 @@ type StoreType = {
     AppComponent,
     AboutComponent,
     HomeComponent,
+    LeashedInHomeComponent,
+    SettingsDialogComponent,
     NoContentComponent,
     XLargeDirective
+  ],
+  entryComponents: [
+    AppComponent,
+    SettingsDialogComponent
   ],
   imports: [ // import Angular's modules
     BrowserModule,
