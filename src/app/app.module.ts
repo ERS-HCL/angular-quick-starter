0ce6552ca7be278f454a94cdfbf44fb7cffc4fba
tsukhu <tsukhu@hcl.com>
2017-05-16 12:40:51 +0530

    formatting update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -99,7 +99,8 @@ const initialReducerState = {
         user: userInitState,
         order: orderInitState
       };
-const appReducer = compose(localStorageSync({keys: ['user'], rehydrate : true}), combineReducers)(reducers);
+const appReducer = compose(
+  localStorageSync({keys: ['user'], rehydrate : true}), combineReducers)(reducers);
 
 export function rootReducer(state: any, action: any) {
   return appReducer(state, action);
